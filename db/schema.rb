dc633006f55428fc468b59632b63c98564f2b6b9
Volha Ramanouskaya <olga.romanovskaya95@gmail.com>
2016-03-27 17:42:47 +0300

    add different navbars to users/guests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327130911) do
+ActiveRecord::Schema.define(version: 20160327142838) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160327130911) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
