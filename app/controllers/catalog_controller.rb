f83cff9d7c69a06b760bfdbda4b8d4cd7584b5a2
Volha Ramanouskaya <olga.romanovskaya95@gmail.com>
2016-04-03 15:44:42 +0300

    Added files via upload

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,5 +1,7 @@
 class CatalogController < ApplicationController
   def index
-    @books = Book.order('created_at DESC').all
+    @chapters = Chapter.order('created_at DESC').all.where('chapters.book_id IS NOT NULL')
+
   end
 end
+
